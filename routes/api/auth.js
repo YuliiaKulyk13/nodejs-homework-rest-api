170e1722c3529753c8fa6c272387d9c84b4cf475
YuliiaKulyk13 <kulyk.yuliia13@gmail.com>
2023-06-19 07:30:03 -0400

    adding user

---
+++ routes/api/auth.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const validateBody = require("../../middlewares/validateBody");
+
+const { schemas } = require("../../models/user");
+
+const ctrl = require("../../controllers/auth");
+
+const router = express.Router();
+
+router.post("/register", validateBody(schemas.registerSchema), ctrl.register);
+
+module.exports = router;
