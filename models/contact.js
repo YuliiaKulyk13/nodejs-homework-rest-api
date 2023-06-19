b8e785086407d83f426279824235610ff72550c0
YuliiaKulyk13 <kulyk.yuliia13@gmail.com>
2023-06-19 07:34:26 -0400

    add owner

--- models/contact.js
+++ models/contact.js
@@ -33,29 +33,37 @@ const Contact = model("contact", contactSchema);
 
 contactSchema.post("save", handleMongooseError);
 
-const addSchema = Joi.object({
-  name: Joi.string()
-    .min(2)
-    .max(15)
-    .messages({ "any.required": `missing required name field` })
-    .required(),
-  email: Joi.string()
-    .email()
-    .messages({
-      "any.required": `missing required email field`,
-    })
-    .required(),
-  phone: Joi.string()
-    .min(5)
-    .max(15)
-    .messages({
-      "any.required": `missing required phone field`,
-    })
-    .required(),
-  favorite: Joi.boolean().messages({
-    "any.required": `missing field favorite`,
-  }),
-});
+const addSchema = Joi.object(
+  {
+    name: Joi.string()
+      .min(2)
+      .max(15)
+      .messages({ "any.required": `missing required name field` })
+      .required(),
+    email: Joi.string()
+      .email()
+      .messages({
+        "any.required": `missing required email field`,
+      })
+      .required(),
+    phone: Joi.string()
+      .min(5)
+      .max(15)
+      .messages({
+        "any.required": `missing required phone field`,
+      })
+      .required(),
+    favorite: Joi.boolean().messages({
+      "any.required": `missing field favorite`,
+    }),
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "user",
+      required: true,
+    },
+  },
+  { versionKey: false, timestamps: true }
+);
 
 const updateFavoriteSchema = Joi.object({
   favorite: Joi.boolean().required().messages({
