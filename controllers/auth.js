530322068e1d6912fd4cf89a332feb7fdc6691d6
YuliiaKulyk13 <kulyk.yuliia13@gmail.com>
2023-06-20 06:48:48 -0400

    add login

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,15 +1,18 @@
 const { User } = require("../models/user");
+const bcrypt = require("bcrypt");
 const { ctrlWrapper, HttpError } = require("../helpers");
 
 const register = async (req, res, next) => {
-  const { email } = req.body;
+  const { email, password } = req.body;
   const user = await User.findOne({ email });
 
   if (user) {
     throw HttpError(409, "Email in use");
   }
 
-  const newUser = await User.create(req.body);
+  const hashPassword = await bcrypt.hash(password, 10);
+
+  const newUser = await User.create({ ...req.body, password: hashPassword });
 
   res.status(201).json({
     email: newUser.email,
@@ -17,6 +20,26 @@ const register = async (req, res, next) => {
   });
 };
 
+const login = async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+  if (!user) {
+    throw HttpError(401, "Email or password is wrong");
+  }
+  const passwordCompare = await bcrypt.compare(password, user.password);
+  if (!passwordCompare) {
+    throw HttpError(401, "Email or password is wrong");
+  }
+
+  const token = "";
+
+  res.json({
+    token,
+  });
+};
+
 module.exports = {
   register: ctrlWrapper(register),
+  login: ctrlWrapper(login),
 };
