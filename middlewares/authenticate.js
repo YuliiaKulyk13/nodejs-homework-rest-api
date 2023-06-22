9b61f8a1dfa80efb8153ad8fcc1ef6f40026c2ff
YuliiaKulyk13 <kulyk.yuliia13@gmail.com>
2023-06-22 15:41:05 -0400

    a

--- middlewares/authenticate.js
+++ middlewares/authenticate.js
@@ -7,11 +7,11 @@ const authenticate = async (req, res, next) => {
   const { authorization = "" } = req.headers;
 
   const [bearer, token] = authorization.split(" ");
-  if (bearer !== "Bearer") {
-    next(HttpError(401, "Not authorized"));
-  }
-
   try {
+    if (bearer !== "Bearer") {
+      next(HttpError(401, "Not authorized"));
+    }
+
     const { id } = jwt.verify(token, SECRET_KEY);
     const user = await User.findById(id);
     if (!user || !user.token || user.token !== token) {
