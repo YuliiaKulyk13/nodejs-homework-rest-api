b196ac0a8423973d8512780847053fdfaf1731dc
YuliiaKulyk13 <kulyk.yuliia13@gmail.com>
2023-06-27 15:10:52 -0400

    sc

--- controllers/login.test.js
+++ controllers/login.test.js
@@ -1,43 +1,33 @@
-// const request = require("supertest");
-// const mongoose = require("mongoose");
-// require("dotenv").config();
-// const { DB_HOST, PORT = 3000 } = process.env;
+const request = require("supertest");
+const mongoose = require("mongoose");
+require("dotenv").config();
+const { DB_HOST, PORT = 3000 } = process.env;
 
-// const app = require("../app");
+const app = require("../app");
 
-// beforeEach(async () => {
-//   await mongoose
-//     .connect(DB_HOST)
-//     .then(() => {
-//       app.listen(PORT, () => {
-//         console.log("Database connection successful");
-//       });
-//     })
-//     .catch((error) => console.log(error.message));
-// });
+beforeEach(async () => {
+  await mongoose.connect(DB_HOST).then(() => {
+    app.listen(PORT, () => {
+      console.log("Database connection successful");
+    });
+  });
+});
 
-// afterEach(async () => {
-//   await mongoose
-//     .disconnect(DB_HOST)
-//     .then(() => console.log("Database disconnection succesful"))
-//     .catch((error) => console.log(error.message));
-// });
+afterEach(async () => {
+  await mongoose.connection.close();
+});
 
-// describe("POST /users/login", () => {
-//   test("Login user", async () => {
-//     const res = await request(app)
-//       .post("/users/login")
-//       .send({
-//         email: "example@example.com",
-//         password: "passwordexample123455$",
-//       })
-//       .catch((error) => console.log(error.message));
+describe("POST /api/users/login", () => {
+  test("Login user", async () => {
+    const res = await request(app).post("/api/users/login").send({
+      email: "example@example.com",
+      password: "passwordexample123455$",
+    });
 
-//     expect(res.status).toBe(200);
-//     expect(typeof res.body.user === "object").toBe(true);
-
-//     expect(typeof res.body.user.email).toBe("string");
-//     expect(typeof res.body.user.subscription).toBe("string");
-//     expect(res.body.token).toBeTruthy();
-//   });
-// });
+    expect(res.status).toBe(200);
+    expect(typeof res.body === "object").toBe(true);
+    expect(typeof res.body.user.email).toBe("string");
+    expect(typeof res.body.user.subscription).toBe("string");
+    expect(res.body.token).toBeTruthy();
+  });
+});
